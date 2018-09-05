8b8674591bd4e67c377ff60579bd55aa6dbe125f
elec34g <elec34g@gmail.com>
2018-09-05 12:59:47 +0000

    fix bug

--- snmp/bin/htu21.sh
+++ snmp/bin/htu21.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-#echo  'Temp 23.64 C;Humidity 40.26%'
-"/opt/orangepizero-temperature/htu21-sensor/build/htu21-orangepi-test.elf t"
 
 #Find the script base path (used to find mapping file)
-#SCRIPT_PATH="`dirname \"$0\"`"              # relative
-#SCRIPT_PATH="`( cd \"$SCRIPT_PATH\" && pwd )`"  # absolutized and normalized
-#if [ -z "$SCRIPT_PATH" ] ; then
+SCRIPT_PATH="`dirname \"$0\"`"              # relative
+SCRIPT_PATH="`( cd \"$SCRIPT_PATH\" && pwd )`"  # absolutized and normalized
+if [ -z "$SCRIPT_PATH" ] ; then
   # error; for some reason, the path is not accessible
   # to the script (e.g. permissions re-evaled after suid)
-#  exit 1  # fail
-#fi
+  exit 1  # fail
+fi
 
-#if [ "$1" = "-g" ]; then
-	#deviceid=$(<$SCRIPT_PATH/../mapping/$2)
+if [ "$1" = "-t" ]; then
+	#deviceid=$(<$SCRIPT_PATH/../mapping/$2
+	/opt/orangepizero-temperature/htu21-sensor/build/htu21-orangepi-test.elf "t"
+else
+	/opt/orangepizero-temperature/htu21-sensor/build/htu21-orangepi-test.elf "h"
+fi
